3af8b55081c10b8639deb6054f139994347ca080
Aman-1-1 <amanregmi10@gmail.com>
2025-11-05 16:14:07 +0545

    some basic changes

--- blooddonation/models.py
+++ blooddonation/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class logindoner(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=256)  # store hashed password
     blood_type = models.CharField(max_length=10)
